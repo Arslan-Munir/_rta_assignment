39e15e6c878a9a14baeca8af679eab51ffc54c96
marslanmunir <arslanmunir987@gmail.com>
2021-12-20 00:21:13 +0500

    Added login post api request

--- _client/src/App.js
+++ _client/src/App.js
@@ -5,28 +5,7 @@ import Routes from "./Routes";
 
 function App() {
   return (
-    <div className="App container py-3">
-      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        {/* <LinkContainer to="/"> */}
-        <Navbar.Brand className="font-weight-bold text-muted">
-          Home
-        </Navbar.Brand>
-        {/* </LinkContainer> */}
-        <Navbar.Toggle />
-
-        <Navbar.Collapse className="justify-content-end">
-          <Nav activeKey={window.location.pathname}>
-            {/* <LinkContainer to="/signup"> */}
-            <Nav.Link href="/signup">Signup</Nav.Link>
-            {/* </LinkContainer> */}
-
-            {/* <LinkContainer to="/login"> */}
-            <Nav.Link href="/login">Login</Nav.Link>
-            {/* </LinkContainer> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
+    <div className="App">
       <Routes />
     </div>
   );
--- _client/src/containers/login/Login.js
+++ _client/src/containers/login/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import "./Login.css";
+import Image from "react-bootstrap/Image";
+import Api from "../../configs/Api";
 
 export default function Login() {
   const [userName, setUsername] = useState("");
@@ -11,41 +13,63 @@ export default function Login() {
   }
 
   function handleSubmit(event) {
-    console.log(userName, password);
+    Api.post("auth", {
+      username: "arslan-munir",
+      password: "fakePass-123",
+    }).then((res) => {
+      console.log(res.data);
+    });
+
     event.preventDefault();
   }
 
   return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg">
-          <Form.Label className="font-weight-bold text-muted">
-            Username
-          </Form.Label>
-          <Form.Control
-            autoFocus
-            type="text"
-            value={userName}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </Form.Group>
+    <div className="container">
+      <div className="d-flex flex-row align-items-center justify-content-center">
+        <div className="card col col-sm col-lg-5 col-md-8">
+          <Form onSubmit={handleSubmit}>
+            <Image
+              className="m-3 img-responsive rounded-circle mx-auto d-block"
+              src="https://bulma.io/images/placeholders/128x128.png"
+            />
+            <Form.Group size="lg">
+              <Form.Control
+                placeholder="Username"
+                autoFocus
+                type="text"
+                value={userName}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Form.Text className="text-muted" size="sm">
+                Registered user: arslan-munir
+              </Form.Text>
+            </Form.Group>
 
-        <Form.Group size="lg">
-          <Form.Label className="font-weight-bold text-muted">
-            Password
-          </Form.Label>
-          <Form.Control
-            autoFocus
-            type="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
+            <Form.Group size="lg">
+              <Form.Control
+                placeholder="Password"
+                autoFocus
+                type="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Form.Text className="text-muted" size="sm">
+                Password: fakePass-123
+              </Form.Text>
+            </Form.Group>
 
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
+            <div className="text-center">
+              <Button
+                className="btn btn-secondary m-2"
+                type="submit"
+                disabled={!validateForm()}
+              >
+                Login
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
